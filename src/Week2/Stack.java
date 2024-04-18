4c74f7a18c386e223f1eef2beeec872f803b4dfd
Andrea Ellison <andreaellison@Andreas-MBP-2.attlocal.net>
2024-04-17 21:06:10 -0400

    Updated with full implementation of linkedlist

--- src/Week2/Stack.java
+++ src/Week2/Stack.java
@@ -1,16 +1,33 @@
 package src.Week2;
 
 public class Stack {
-  private class Node {
+  private class Node 
+  {
     String item;
     Node next;
   }
-  private Node first;
-  public static void main(String[] args) {  }
+  private Node first = null;
   
-  public String pop() {
-    String item = first.item;
+  public static void main(String[] args) {  }
+
+  public boolean isEmpty() 
+  {
+    return first == null;
+  }
+  
+  public String pop() 
+  {
+    String item = first.item; //will be gc
     first = first.next;
     return item;
   }
+
+  public void push(String str) 
+  {
+    Node oldfirst = first; 
+    first = new Node();
+    first.item = str;
+    first.next = oldfirst; // now second
+  }
+
 }
